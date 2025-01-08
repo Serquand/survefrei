a4c6e191f2ccd4dddacd456b9d143cdd3672733e
Serquand <esteban.vincent@efrei.net>
2025-01-09 00:44:19 +0100

    Fix bug

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -70,14 +70,22 @@ const LoginPage = () => {
             body: JSON.stringify({ email }),
             headers: { "Content-Type": "application/json" }
         }
-        const response = await fetch(apiUrl + '/auth/forgot-password', requestOptions);
-        const informations = await response.json();
-        if (!response.ok) throw new Error(informations.message);
 
-        setNotificationTitle("Information")
-        setNotificationInformations("A password reset link has been sent to your email, if your email was registered in the system");
-        setNotificationsSuccess(true);
-        showNotification();
+        try {
+            const response = await fetch(apiUrl + '/auth/forgot-password', requestOptions);
+            if (!response.ok) throw new Error();
+
+            setNotificationTitle("Information")
+            setNotificationInformations("A password reset link has been sent to your email, if your email was registered in the system");
+            setNotificationsSuccess(true);
+            setMode("login")
+        } catch {
+            setNotificationTitle("Error")
+            setNotificationInformations("Something went wrong!");
+            setNotificationsSuccess(true);
+        } finally {
+            showNotification();
+        }
     }
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -93,19 +101,7 @@ const LoginPage = () => {
     };
 
     return (
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <img
-                    className="mx-auto h-14 w-auto"
-                    src="/Logo SurvEfrei.png"
-                    alt="Your Company"
-                />
-
-                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                    {mode === 'login' ? "Sign in to your account" : "Retrieve your password"}
-                </h2>
-            </div>
-
+        <>
             {isLoading && <Loader />}
             <Notification
                 ref={notificationRef}
@@ -118,86 +114,100 @@ const LoginPage = () => {
                 }
             </Notification>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form
-                    className="space-y-6"
-                    method="POST"
-                    onSubmit={handleSubmit}
-                >
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label
-                                htmlFor="email"
-                                className="block cursor-pointer text-sm/6 font-medium text-gray-900"
-                            >
-                                Email adress
-                            </label>
+            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        className="mx-auto h-14 w-auto"
+                        src="/Logo SurvEfrei.png"
+                        alt="Your Company"
+                    />
 
-                            {mode === 'retrieve_password' && <div
-                                className="text-sm cursor-pointer font-semibold text-indigo-500 hover:text-indigo-700"
-                                onClick={() => setMode('login')}
-                            >
-                                Remember password?
-                            </div>}
-                        </div>
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                        {mode === 'login' ? "Sign in to your account" : "Retrieve your password"}
+                    </h2>
+                </div>
 
-                        <div className="mt-2">
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                required
-                                value={email}
-                                placeholder="Your email adress"
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
-                        </div>
-                    </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form
+                        className="space-y-6"
+                        method="POST"
+                        onSubmit={handleSubmit}
+                    >
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="email"
+                                    className="block cursor-pointer text-sm/6 font-medium text-gray-900"
+                                >
+                                    Email adress
+                                </label>
 
-                    {mode === 'login' && <div>
-                        <div className="flex items-center justify-between">
-                            <label
-                                htmlFor="password"
-                                className="block text-sm/6 font-medium text-gray-900"
-                            >
-                                Password
-                            </label>
+                                {mode === 'retrieve_password' && <div
+                                    className="text-sm cursor-pointer font-semibold text-indigo-500 hover:text-indigo-700"
+                                    onClick={() => setMode('login')}
+                                >
+                                    Remember password?
+                                </div>}
+                            </div>
 
-                            <div
-                                className="text-sm cursor-pointer font-semibold text-indigo-500 hover:text-indigo-700"
-                                onClick={() => setMode('retrieve_password')}
-                            >
-                                Forgot password?
+                            <div className="mt-2">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    required
+                                    value={email}
+                                    placeholder="Your email adress"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                />
                             </div>
                         </div>
 
-                        <div className="mt-2">
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                autoComplete="current-password"
-                                required
-                                placeholder="Your password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
-                        </div>
-                    </div>}
+                        {mode === 'login' && <div>
+                            <div className="flex items-center justify-between">
+                                <label
+                                    htmlFor="password"
+                                    className="block text-sm/6 font-medium text-gray-900"
+                                >
+                                    Password
+                                </label>
 
-                    <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            {mode === 'login' ? "Sign in" : "Reset password"}
-                        </button>
-                    </div>
-                </form>
+                                <div
+                                    className="text-sm cursor-pointer font-semibold text-indigo-500 hover:text-indigo-700"
+                                    onClick={() => setMode('retrieve_password')}
+                                >
+                                    Forgot password?
+                                </div>
+                            </div>
+
+                            <div className="mt-2">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    required
+                                    placeholder="Your password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>}
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                {mode === 'login' ? "Sign in" : "Reset password"}
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
